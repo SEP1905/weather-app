c8eea46546ba8cd510055a6956c43e010c433da4
SEP1905 <shaniyahpalmer07@gmail.com>
2023-03-14 21:18:09 +0000

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ function displayWeatherCondition(response) {
         "src",
         `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`    
     );
-    iconElement.setAttribute("alt", response.data.weather[0].description);
+    iconElement.setAttribute("alt", response.data.weather[0].description); 
 
      celsiusTemperature = response.data.main.temp;
 }
